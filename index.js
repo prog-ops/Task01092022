539de22c7a29f80a94e7cbbd7f92d226fd3ec555
prgrm274 <programmer270487@gmail.com>
2022-09-06 16:11:37 +0700

    update index

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux'
+import StoreManager from './src/configs/StoreManager';
+
+const store = StoreManager()
+
+const Application = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Application);
